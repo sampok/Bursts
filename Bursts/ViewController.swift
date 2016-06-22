0727ee16a9899e059d844b04acc473e03a879235
Sampo Karjalainen <skarjalainen@fb.com>
2016-06-21 18:25:48 -0700

    Initial Commit

---
+++ Bursts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bursts
+//
+//  Created by Sampo Karjalainen on 6/21/16.
+//  Copyright © 2016 Team Yellow 3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
